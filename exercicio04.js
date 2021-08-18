60b1893f8f75d8386d8d01e5a0db0dd0859f0a04
Nycolas Barbosa <nyckbarbosacel@gmail.com>
2021-08-17 23:49:47 -0300

    exercicio 04

---
+++ exercicio04.js
@@ -0,0 +1,9 @@
+nomeCompleto = "Nycolas André de Oliveira barbosa"
+apelido = "Nyck"
+idade = 26
+dataNascimento = "30/10/1994"
+localNascimento = "canoas"
+altura = 1.83
+trabalhando = true
+apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."
+console.log(apresentacao)
